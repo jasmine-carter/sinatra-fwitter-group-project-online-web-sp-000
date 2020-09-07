7e280d2c05f3449d81f584cf6d8a9e52ce510075
Jasmine Carter <jasmine.carter87@gmail.com>
2020-09-07 20:54:38 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,12 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    @user = current_user
+    if !logged_in?
+      redirect "/login"
+    else
+      erb :'/index'
+    end
+  end
 
 end
