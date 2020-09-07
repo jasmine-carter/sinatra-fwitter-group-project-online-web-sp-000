09dd470170ace0b6fe1a9cc02006253e62318fee
Jasmine Carter <jasmine.carter87@gmail.com>
2020-09-07 22:31:36 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -22,12 +22,12 @@ class TweetsController < ApplicationController
   post '/tweets' do
     binding.pry
     if params[:content] == nil || params[:content] ==" " || params[:content] == ""
+      flash[:message] = "Please create a tweet with content!"
+      redirect "/tweets/new"
+    else
       @tweet = Tweet.new(content: params["content"], user_id: session[:user_id])
       @tweet.save
       redirect "/tweets"
-    else
-      flash[:message] = "Please create a tweet with content!"
-      redirect "/tweets/new"
     end
   end
 
