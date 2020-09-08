63e4e12c02e9eb3bdbecb1e97f162ef7de0b1967
Jasmine Carter <jasmine.carter87@gmail.com>
2020-09-08 00:04:39 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require 'bundler/setup'
 require 'sysrandom'
 require 'securerandom'
 require 'rack-flash'
-ENV["SESSION_SECRET"] = SecureRandom.hex(64)
+#ENV["SESSION_SECRET"] = SecureRandom.hex(64)
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
