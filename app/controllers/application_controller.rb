f5fd032740f5862604639e49a0e4d3f07b2e7783
Jasmine Carter <jasmine.carter87@gmail.com>
2020-09-08 00:04:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, ENV.fetch('SESSION_SECRET')
+    #set :session_secret, ENV.fetch('SESSION_SECRET')
+    set :session_secret, SecureRandom.hex(64)
   end
 
   get '/' do
